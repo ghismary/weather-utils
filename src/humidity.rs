4a277bb4342e9e0d19fc08acc480d7547ec08584
Ghislain MARY <ghislain@ghislainmary.fr>
2026-06-09 00:02:36 +0200

    feat: add heat index computation

--- src/humidity.rs
+++ src/humidity.rs
@@ -1,3 +1,5 @@
+use core::ops::Deref;
+
 #[allow(unused_imports)]
 #[cfg(feature = "no-std")]
 use micromath::F32Ext;
@@ -45,6 +47,57 @@ impl PartialEq for RelativeHumidity {
     }
 }
 
+/// The heat index.
+///
+/// The heat index indicates how the human body feels temperature. If relative humidity is low
+/// human body cools itself by perspiration, dissipating heat from the body. At higher
+/// relative humidity the evaporation rate from the human skin is lower. In that case, the
+/// body cannot dissipate heat as easily as it is the case in dry air.
+/// The heat index is based on subjective measurements and is only meaningful above 25°C and
+/// 40% RH.
+#[derive(Clone, Copy, Debug, Default)]
+pub struct HeatIndex<T: Temperature>(T);
+
+impl<T: Temperature> HeatIndex<T> {
+    /// Get the relative human body comfort corresponding to the heat index.
+    pub fn comfort(&self) -> Comfort {
+        if self.celsius().value() < 30. {
+            Comfort::NoDiscomfort
+        } else if self.celsius().value() < 40. {
+            Comfort::SomeDiscomfort
+        } else if self.celsius().value() < 45. {
+            Comfort::GreatDiscomfort
+        } else if self.celsius().value() < 54. {
+            Comfort::Dangerous
+        } else {
+            Comfort::HeatStrokeImminent
+        }
+    }
+}
+
+impl<T: Temperature> Deref for HeatIndex<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+/// The relative human body comfort corresponding to a heat index.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Comfort {
+    /// No discomfort: heat index below 30°C.
+    NoDiscomfort,
+    /// Some discomfort: heat index between 30°C and 40°C.
+    SomeDiscomfort,
+    /// Great discomfort: heat index between 40°C and 45°C.
+    GreatDiscomfort,
+    /// Dangerous: heat index between 45°C and 54°C.
+    Dangerous,
+    /// Heat stroke immiment: heat index above 54°C.
+    HeatStrokeImminent,
+}
+
 /// The combination of the temperature and the relative humidity.
 #[derive(Clone, Copy, Debug, Default)]
 pub struct TemperatureAndRelativeHumidity<T: Temperature> {
@@ -77,6 +130,47 @@ impl<T: Temperature> TemperatureAndRelativeHumidity<T> {
             + ((M * self.temperature.celsius().value())
                 / (TN + self.temperature.celsius().value()));
         T::from_celsius(Celsius((TN * val) / (M - val)))
+    }
+
+    /// Computes the heat index.
+    ///
+    /// See `HeatIndex`.
+    pub fn heat_index(&self) -> HeatIndex<T> {
+        const C1: f32 = -8.784_695;
+        const C2: f32 = 1.611_394_2;
+        const C3: f32 = 2.338_549;
+        const C4: f32 = -0.146_116_05;
+        const C5: f32 = -0.012_308_094;
+        const C6: f32 = -0.016_424_827;
+        const C7: f32 = 0.002_211_732;
+        const C8: f32 = 0.000_725_46;
+        const C9: f32 = -0.000_003_582;
+
+        let temperature = self.temperature.celsius().value();
+        let relative_humidity = self.relative_humidity.value();
+        let mut heat_index = 1.1 * temperature + 5. * (0.047 * relative_humidity - 7.1) / 9.;
+        if (heat_index + temperature) / 2. >= 26.7 {
+            heat_index = C1
+                + C2 * temperature
+                + C3 * relative_humidity
+                + C4 * temperature * relative_humidity
+                + C5 * temperature * temperature
+                + C6 * relative_humidity * relative_humidity
+                + C7 * temperature * temperature * relative_humidity
+                + C8 * temperature * relative_humidity * relative_humidity
+                + C9 * temperature * temperature * relative_humidity * relative_humidity;
+            if relative_humidity < 13. && temperature > 26.7 && temperature < 44.4 {
+                heat_index += (5. / 36.)
+                    * (relative_humidity - 13.)
+                    * ((17. - (1.8 * temperature - 63.).abs()) / 17.).sqrt()
+                    - 160. / 9.;
+            }
+            if relative_humidity > 85. && temperature > 26.7 && temperature < 30.6 {
+                heat_index +=
+                    5. * (relative_humidity - 85.) * (55. - 1.8 * temperature) / 450. - 160. / 9.;
+            }
+        }
+        HeatIndex(T::from_celsius(Celsius(heat_index)))
     }
 }
 
@@ -162,6 +256,31 @@ mod tests {
             expected_dew_point.value(),
             epsilon = 0.01
         );
+    }
+
+    #[rstest]
+    #[case(TemperatureAndRelativeHumidity{ temperature: Celsius(27.), relative_humidity: RelativeHumidity::new(40.).unwrap() }, Celsius(26.86), Comfort::NoDiscomfort)]
+    #[case(TemperatureAndRelativeHumidity{ temperature: Celsius(29.), relative_humidity: RelativeHumidity::new(50.).unwrap() }, Celsius(29.65), Comfort::NoDiscomfort)]
+    #[case(TemperatureAndRelativeHumidity{ temperature: Celsius(31.), relative_humidity: RelativeHumidity::new(60.).unwrap() }, Celsius(34.84), Comfort::SomeDiscomfort)]
+    #[case(TemperatureAndRelativeHumidity{ temperature: Celsius(32.), relative_humidity: RelativeHumidity::new(70.).unwrap() }, Celsius(40.41), Comfort::GreatDiscomfort)]
+    #[case(TemperatureAndRelativeHumidity{ temperature: Celsius(34.), relative_humidity: RelativeHumidity::new(80.).unwrap() }, Celsius(52.2), Comfort::Dangerous)]
+    #[case(TemperatureAndRelativeHumidity{ temperature: Celsius(36.), relative_humidity: RelativeHumidity::new(90.).unwrap() }, Celsius(69.2), Comfort::HeatStrokeImminent)]
+    #[case(TemperatureAndRelativeHumidity{ temperature: Celsius(37.5), relative_humidity: RelativeHumidity::new(100.).unwrap() }, Celsius(88.71), Comfort::HeatStrokeImminent)]
+    #[case(TemperatureAndRelativeHumidity{ temperature: Fahrenheit(80.6), relative_humidity: RelativeHumidity::new(40.).unwrap() }, Fahrenheit(80.346), Comfort::NoDiscomfort)]
+    #[case(TemperatureAndRelativeHumidity{ temperature: Fahrenheit(89.6), relative_humidity: RelativeHumidity::new(70.).unwrap() }, Fahrenheit(104.738), Comfort::GreatDiscomfort)]
+    #[case(TemperatureAndRelativeHumidity{ temperature: Fahrenheit(96.8), relative_humidity: RelativeHumidity::new(90.).unwrap() }, Fahrenheit(156.56), Comfort::HeatStrokeImminent)]
+    fn test_heat_index_computation<T: Temperature>(
+        #[case] input: TemperatureAndRelativeHumidity<T>,
+        #[case] expected_heat_index: T,
+        #[case] expected_comfort: Comfort,
+    ) {
+        let heat_index = input.heat_index();
+        assert_relative_eq!(
+            heat_index.value(),
+            expected_heat_index.value(),
+            epsilon = 0.01
+        );
+        assert_eq!(heat_index.comfort(), expected_comfort);
     }
 
     #[rstest]
